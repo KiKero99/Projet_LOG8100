24fbdf017d79586b030c6e4fad683f7d38a9c8e6
KiKero99 <enrique.fashaton@gmail.com>
2024-12-04 19:42:54 -0500

    add terraform and ansible
    missing workflow

--- main.tf
+++ main.tf
@@ -1,29 +1,52 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.8"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 1.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-resource-group"
+  location = "eastus"
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks-cluster"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks"
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "akscluster"
 
   default_node_pool {
     name       = "default"
-    node_count = 3
+    node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
 
   identity {
     type = "SystemAssigned"
   }
-}
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.raw_kube_config
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
 }
